ede5e3b834d76d74536689c8c760ce6e97df5127
andrade-matheus <mat.tonelliandrade@gmail.com>
2019-03-30 13:50:05 -0400

    Codigo nao funcionando do RFID

--- Projeto.ino
+++ Projeto.ino
@@ -1,8 +1,16 @@
 #include <LiquidCrystal.h>
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 6
+#define RST_PIN 2
 
-LiquidCrystal lcd(12,11,5,4,3,2);
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+LiquidCrystal lcd(13,12,11,10,9,8);
 
-long unsigned int tempo_inicial, tempo_final, tempo_total;
+float tempo_inicial = 0;
+float tempo_final = 0;
+float tempo_total = 0;
 float vetCorrente[300];
 float tensao = 127;
 double corrente = 0;
@@ -10,17 +18,64 @@ double maior_Valor = 0;
 double valor_Corrente = 0; 
 double potencia = 0;
 double energia = 0;
+double energia_ant = 0;
+double calculo = 0;
+
+String conteudo= "";
+
+void RFID(){
+  Serial.println("TESTE RFID");
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Mostra UID na serial
+  Serial.print("UID da tag :");
+  
+  
+  while(conteudo.substring(1) != "FB C0 4C 0C" || conteudo.substring(1) != "16 DB 28 F8"){
+    byte letra;
+    for (byte i = 0; i < mfrc522.uid.size; i++) 
+    {
+       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+       Serial.print(mfrc522.uid.uidByte[i], HEX);
+       conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+       conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+    Serial.println();
+    Serial.print("Mensagem : ");
+    conteudo.toUpperCase();
+    Serial.println(conteudo.substring(1)); 
+  }
+  digitalWrite(7, HIGH);  
+}
 
 void setup() {
 	Serial.begin(9600);
-	pinMode(A0, INPUT);
+	pinMode(A3, INPUT);
 	lcd.begin(16, 2);
+ 
+  SPI.begin();      // Inicia  SPI bus
+  mfrc522.PCD_Init();   // Inicia MFRC522
 
+  pinMode(7, OUTPUT);
+
+  Serial.println("Aproxime o seu cartao do leitor...");
+  Serial.println();
+  
+  RFID();
 }
 
 void loop() {
+  Serial.println("TESTE LOOP");
 	for(int i = 0; i < 300; i++) {
-		corrente = analogRead(A0);
+		corrente = analogRead(A2);
 		if(corrente > maior_Valor) {
 			maior_Valor = corrente;
 		}
@@ -33,12 +88,13 @@ void loop() {
 	valor_Corrente = valor_Corrente / 1.41421356;
 
 	tempo_final = tempo_inicial;
-	tempo_inicial = milis();
-	tempo_total = tempo_final - tempo_inicial;
-
+	tempo_inicial = millis();
+	tempo_total = tempo_inicial - tempo_final;
+  
 	potencia = 110 * valor_Corrente;
-	energia += potencia * (tempo_total/60/60/1000);
-
+  
+	energia += (potencia * (tempo_total/60/60/1000));
+  
 	//Imprime no LCD
 	lcd.setCursor(0,0);
 	lcd.print(valor_Corrente);
@@ -52,18 +108,29 @@ void loop() {
 	lcd.print(energia);
 	lcd.print(" kWh");
 
-	//Imprime no Terminal
-	Serial.print("Corrente = ");
-	Serial.print(valor_Corrente);
-	Serial.println(" A");
-
-	Serial.print("Potencia = ");
-	Serial.print(potencia);
-	Serial.println(" W");
-
-	Serial.print("Energia = ");
-	Serial.print(energia);
-	Serial.println(" kWh");
-
 	delay(500);
+
+  Serial.println("TESTE LOOP 2");
+  
+  byte letra;
+  String conteudo_loop = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+    Serial.println("TESTE LOOP 3");
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     conteudo_loop.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     conteudo_loop.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  Serial.println("TESTE LOOP 4");
+  
+  if(conteudo_loop.equals(conteudo)){
+    digitalWrite(7, LOW);
+    delay(1000);
+    RFID();     
+  }
+
+  Serial.println("TESTE LOOP 5");
+  
 }
