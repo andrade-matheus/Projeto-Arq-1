a19d6e8b0b4b8f08673d15886fad3c1cec7d767c
andrade-matheus <mat.tonelliandrade@gmail.com>
2019-04-10 10:40:55 -0400

    Codigo pronto

--- Projeto.ino
+++ Projeto.ino
@@ -1,24 +1,23 @@
-#include "ACS712.h"
 #include <LiquidCrystal.h>
 #include <SPI.h>
-#include <MFRC522.h>
- 
+#include <MFRC522.h> 
+#include "ACS712.h"
+
 #define SS_PIN 10
 #define RST_PIN 9
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-LiquidCrystal lcd(3,4,5,6,7,8);
 
-ACS712 sensor(ACS712_30A, A0);
+LiquidCrystal lcd(3,4,5,6,7,8);     //Instancia do LCD
+
+ACS712 sensor(ACS712_05B, A0);
 
 float tempo_inicial = 0;
 float tempo_final = 0;
 float tempo_total = 0;
-float vetCorrente[300];
-float tensao = 127;
-double corrente = 0;
-double maior_Valor = 0;
-double valor_Corrente = 0; 
+
+double corrente = 0; 
+
 double potencia = 0;
 double energia = 0;
 double energia_ant = 0;
@@ -29,51 +28,62 @@ String comparar = "";
 
 bool liberou = false;
 
+int tensao = 110;
+
+float preco_Wh = 0.614320;
+
+float consumo = 0;
+float gasto = 0;
+
 void setup() {
   Serial.begin(9600);
 
-  Serial.println("Calibrating... Ensure that no current flows through the sensor at this moment");
-  sensor.calibrate();
-  Serial.println("Done!");
-  
   lcd.begin(16, 2);
- 
+
   SPI.begin();      // Inicia  SPI bus
   mfrc522.PCD_Init();   // Inicia MFRC522
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  delay(2000);
+  
+  sensor.calibrate();
 }
 
 void loop() {
+  
   if(liberou){
-    /*
-    for(int i = 0; i < 300; i++) {
-      corrente = analogRead(A0);
-      if(corrente > maior_Valor) {
-        maior_Valor = corrente;
-      }
-    }
-  
-    maior_Valor = maior_Valor * 0.004882812;
-    valor_Corrente = maior_Valor - 2.5;
-    valor_Corrente = valor_Corrente * 1000;
-    valor_Corrente = valor_Corrente / 66;         //sensibilidade : 66mV/A para ACS712 30A / 185mV/A para ACS712 5A
-    valor_Corrente = valor_Corrente / 1.41421356;
-    */
+    corrente = -0.114;
+    corrente += sensor.getCurrentAC(60);
     
-    valor_Corrente = sensor.getCurrentAC();
-  
     tempo_final = tempo_inicial;
     tempo_inicial = millis();
     tempo_total = tempo_inicial - tempo_final;
     
-    potencia = 100 * valor_Corrente;
+    potencia = tensao * corrente;
     
-    energia += (potencia * (tempo_total/60/60/1000));
+    energia = (energia + (potencia * (((tempo_total/60)/60)/10000)));
+
+
+    Serial.print("Tempo: ");
+    Serial.println(tempo_total);
+
+    Serial.print("Ampere: ");
+    Serial.println(corrente, 5);
+    
+    Serial.print("Potencia: ");
+    Serial.println(potencia, 5);
+  
+    Serial.print("Energia: ");
+    Serial.println((potencia * (((tempo_total/60)/60)/1000)), 5);
+    Serial.println("----");
 
     //Limpa tela do LCD
     lcd.clear();
     //Imprime no LCD
     lcd.setCursor(0,0);
-    lcd.print(valor_Corrente);
+    lcd.print(corrente);
     lcd.print(" Amp");
     
     lcd.setCursor(9,0);
@@ -94,14 +104,32 @@ void loop() {
         }
         conteudo.toUpperCase();    
       }
-      if((conteudo.substring(1) == "FB C0 4C 0C") || (conteudo.substring(1) == "16 DB 28 F8")){
+      if(conteudo.substring(1) == comparar){
         energia = 0;
-        conteudo = "";
+        comparar = "";
         liberou = false;
         Serial.println("ACESSO ENCERRADO !!!");
-        digitalWrite(2, LOW);
+        digitalWrite(2, HIGH);
         delay(500);
+
+        consumo = energia;
+        gasto = consumo * preco_Wh;
+        
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Consumo: ");
+        lcd.print(consumo);
+        lcd.print("kWh");
+
+        lcd.setCursor(0,1);
+        lcd.print("Gasto: R$");
+        lcd.print(gasto);
+
+        delay(10000);
+        
+        energia = 0;
       }
+      conteudo = "";
     }
   } else {
     lcd.clear();
@@ -121,10 +149,13 @@ void loop() {
         conteudo.toUpperCase();
       }
       if((conteudo.substring(1) == "FB C0 4C 0C") || (conteudo.substring(1) == "16 DB 28 F8")){
+        comparar = conteudo.substring(1);
         conteudo = "";
         liberou = true;
         Serial.println("ACESSO LIBERADO !!!");
-        digitalWrite(2, HIGH);
+
+        //LIGA O RELÉ
+        digitalWrite(2, LOW);
         delay(500);
       }
     }
